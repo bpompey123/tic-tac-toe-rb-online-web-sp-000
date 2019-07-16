2616ac73d6a43fd3796622858e37cca92ad10266
Brian Pompey <brianthescholar@gmail.com>
2019-07-16 23:46:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -122,6 +122,9 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    winner(board) == "X" || winner(board) == "O" 
+    winner(board) == "X" || winner(board) == "O"
+    puts "Congratulations #{winner(board)}!"
+   elsif draw?(board)
+      puts "Cats Game!" 
   end
 end
