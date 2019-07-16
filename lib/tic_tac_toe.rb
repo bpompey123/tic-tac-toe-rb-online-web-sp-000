baada8c691e4150caba99034628ba226b69e6664
Brian Pompey <brianthescholar@gmail.com>
2019-07-16 23:30:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,3 +50,27 @@ def valid_move?(board, index)
     return false
   end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  input = input_to_index(input)
+
+  if valid_move?(board, input)
+    move(board, input, value = "X")
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
+
