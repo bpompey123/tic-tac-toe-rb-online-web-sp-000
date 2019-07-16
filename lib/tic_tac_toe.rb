8cf6ded9b1a956f35e228c2b90ee5ac44d3bb9a5
Brian Pompey <brianthescholar@gmail.com>
2019-07-16 23:28:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,3 +8,46 @@ WIN_COMBINATIONS = [
   [2,5,8],
   [2,4,6]
 ]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input.to_i - 1
+
+end
+
+def move(board, index, value = "X")
+
+  def update_array(array, index, value)
+
+    array[index] = value
+  end
+
+  update_array(board, index, value)
+
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+    
+  end
+end
+
+def valid_move?(board, index)
+  if board[index] == " "
+    return true
+  else
+    return false
+  end
+end
+
